4e30a4cafdfd3620cb3c73af57e0cdf34e58db3e
Clint Shaw <shaw.clint@gmail.com>
2019-01-28 15:12:58 -0800

    looking at thingy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 set -e -x
 
 az login --service-principal --username $APP_ID --password $PASSWORD --tenant $TENANT_ID
-func azure login --username $ACCUSERNAME -password $ACCPASSWORD
+func azure login --username $ACCUSERNAME --password $ACCPASSWORD
 func azure functionapp publish clint-test-demo --build-native-deps --force
